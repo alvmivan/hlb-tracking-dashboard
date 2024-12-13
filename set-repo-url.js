013a3713cbb984a52ac310f2fecad7cda7919caa
Marcos Iván Alvarez <alvmivan@gmail.com>
2024-12-12 22:10:38 -0300

    more debug on preinstall

--- set-repo-url.js
+++ set-repo-url.js
@@ -1,16 +1,23 @@
 ﻿const fs = require('fs');
 const NPM_TOKEN = process.env.NPM_TOKEN;
 
-console.log("Corriendo pre install");
+console.log("Running pre install script");
 
-if (NPM_TOKEN) {
-    
+if (!NPM_TOKEN) {
+    console.error("NPM_TOKEN environment variable is not set");
+    process.exit(1);
+}
+
+try {
     const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
     const dependencyName = 'hlb-api-library';
     const currentVersion = packageJson.dependencies[dependencyName].split('#')[1];
     packageJson.dependencies[dependencyName] = `https://${NPM_TOKEN}@github.com/alvmivan/hlb-api-library.git#${currentVersion}`;
     fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
-    console.log('Updated hlb-api-library URL based on environment');
-}  
+    console.log('Successfully updated hlb-api-library URL with auth token');
+} catch (error) {
+    console.error('Error updating package.json:', error);
+    process.exit(1);
+}
 
-console.log("Fin pre install");
+console.log("Pre install script completed");
