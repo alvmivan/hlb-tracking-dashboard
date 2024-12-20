0fabd4d2e9cc55f3ff59f07ba4ef9596190d4d9e
Marcos Iván Alvarez <alvmivan@gmail.com>
2024-12-20 16:43:24 -0300

    updater funcionando

---
+++ update-submodule.cmd
@@ -0,0 +1,18 @@
+﻿@echo off
+REM Enlazar el repositorio
+cd src\lib\hlb-api-library
+git init
+git remote add origin https://github.com/alvmivan/hlb-api-library
+
+REM Limpiar archivos no rastreados (y directorios)
+git clean -fd
+
+REM Ejecutar git fetch y luego resetear con hard pull
+git fetch origin
+git reset --hard origin/main
+
+REM Eliminar rastro de Git
+rd /s /q .git
+
+REM Volver al directorio original
+cd /d %~dp0
